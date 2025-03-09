a218479217d1ea50f2215a525e2ea684af9076a2
Vijay Raghav Varada <vjvarada@gmail.com>
2025-03-10 00:28:31 +0530

    updated control screen to should RGB feed

--- src/ui/control_screen/control_screen.py
+++ src/ui/control_screen/control_screen.py
@@ -23,7 +23,7 @@ class ControlScreen(QWidget):
 
         self.chamberTempSpinBox = self.findChild(QSpinBox, "chamberTempSpinBox")
         self.setChamberTempButton = self.findChild(QPushButton, "setChamberTempButton")
-        
+        self.cooldownButton = self.findChild(QPushButton, "cooldownButton")
 
         # Setup any signal-slot connections and additional initialization here
         self.setChamberTempButton.clicked.connect(lambda: self.update_setpoint(self.chamberTempSpinBox.value()))
@@ -35,8 +35,19 @@ class ControlScreen(QWidget):
         layout.addWidget(self.thermalCameraWidget)
         self.thermalCameraWidget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
+
+        # Replace the QWidget with the custom ImageWidget
+        rgb_camera_container = self.findChild(QWidget, "rgbCameraWidget")
+        self.rgbCameraWidget = ImageWidget(rgb_camera_container)
+        layout = QVBoxLayout(rgb_camera_container)
+        layout.addWidget(self.rgbCameraWidget)
+        self.rgbCameraWidget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
         # Connect the temperatures_updated signal to the update_thermal_camera_widget slot
         self.main_window.printer_status.temperatures_updated.connect(self.update_thermal_camera_widget)
+        self.main_window.printer_status.rgb_frame_updated.connect(self.update_rgb_camera_widget)
+
+        self.cooldownButton.clicked.connect(self.cooldown)
 
     def update_setpoint(self, value):
         """Update the chamber temperature setpoint in the PrinterStatus model."""
@@ -48,3 +59,14 @@ class ControlScreen(QWidget):
         if frame is not None:
             image = QImage(frame.data, frame.shape[1], frame.shape[0], frame.strides[0], QImage.Format_BGR888)
             self.thermalCameraWidget.setImage(image)
+
+    @pyqtSlot(np.ndarray)
+    def update_rgb_camera_widget(self, frame):
+        if frame is not None:
+            height, width, channel = frame.shape
+            bytes_per_line = 3 * width
+            image = QImage(frame.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
+            self.rgbCameraWidget.setImage(image)
+
+    def cooldown(self):
+        self.main_window.printer_status.chamberTemperatureSetpoint = 0
