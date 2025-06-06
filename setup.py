afab95785648cce5502c65952463a9ad34ba5675
yashaspadma <yyashasy@gmail.com>
2025-06-06 13:55:05 +0530

    MANIFEST
    MANIFEST files added

--- setup.py
+++ setup.py
@@ -44,20 +44,7 @@ plugin_requires = []
 
 # Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
 # already be installed automatically if they exist.
-plugin_additional_data = [
-    "ui/*",
-    "config/*",
-    "Feeltek/*",
-    "firmware/*",
-    "gcode/*",
-    "hurry/*",
-    "models/*",
-    #"moonrakerClient/*",
-    "octoprint_client/*",
-    #"processAutomationController/*",
-    #"temperatureController/*",
-    "utils/*"
-]
+plugin_additional_data = []
 
 # Any additional python packages you need to install with your plugin that are not contains in <plugin_package>.*
 plugin_addtional_packages = []
